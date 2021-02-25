1330fc6b040c94a64e9e67d8349f433aa93e246f
hauk3wu1ff <hauk3wu1ff@gmail.com>
2021-02-25 12:02:45 +0000

    Exercise1 completed.

---
+++ exercise1/go.mod
@@ -0,0 +1,3 @@
+module github.com/hauk3wu1ff/learngo/exercise1
+
+go 1.16
---
+++ exercise1/main.go
@@ -0,0 +1,9 @@
+// Prints my two favourite Birtish actors
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Keith Allen was the Sheriff of Nottingham in 80ties Robin Hood")
+	fmt.Println("John Nettles who starredin both Bergerac and Midsomer Murders")
+}
